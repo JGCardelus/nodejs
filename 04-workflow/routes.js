351a3ecf75f0b175094770588eb31f31fedb6bd8
Jorge Gonzalez <jgcardelus@gmail.com>
2021-08-29 13:02:55 +0200

    Module 4

--- 04-workflow/routes.js
+++ 04-workflow/routes.js
@@ -1,70 +1,74 @@
 const fs = require('fs');
 
-const requestHandler = (request, response) => {
+const requestHandler = (request, response) => {	
+	// Retrieve important information
 	const url = request.url;
 	const method = request.method;
-	
-	if (url === '/form-test') {
-		// Creating the response
+
+	if (url === "/") {
+		// What kind of response are we sending
 		response.setHeader("Content-Type", "text/html");
 		response.write(`
-		<html>
-			<head><title>Sup motherfucker</title></head>
+			<html>
 			<body>
-				<h1>Hello Motherfucker!!!!</h1>
-				<form action="/form-submit" method="POST">
-					<input type="text" name="idk"/>
-					<button type="submit">This is a button</button>
+				<h1>Hi there,</h1>
+				<p>Welcome to my awesome server</p>
+			</body>
+			</html>
+		`);
+		return response.end();
+	} else if (url === "/users") {
+		response.setHeader("Content-Type", "text/html");
+		response.write(`
+			<html>
+			<body>
+				<h1>Listing users</h1>
+				<ul>
+		`);	
+		for (let i = 0; i < 10; i++) {
+			response.write(`
+				<li>Awesome User #${i}</li>
+			`);
+		}
+		response.write(`
+			</uL>
+		</body>
+		</html>
+		`);
+		return response.end();
+	} else if (url === "/create-user") {
+		response.setHeader("Content-Type", "text/html");
+		response.write(`
+			<html>
+			<body>
+				<h1>Create a new user</h1>
+				<h3>(but not really tho)</h3>
+				
+				<form action="/new-user" method="POST">
+					<input type="text" name="username">
+					<button action="submit">Click me to save</button>
 				</form>
 			</body>
-		</html>
+			</html>
 		`);
-	
-		// We return the function so no more code is executed
 		return response.end();
-		// If we keep executing code and access
-		// the response it will throw an error
-	} else if (url === '/form-submit' && method === 'POST') {
-		const body = [];
-		// The request's body is parsed in chunks so that nothing
-		// is blocked
-		// Every time a request's body chunk is parsed we add it
-		// to the body
-		request.on("data", (chunk) =>  {
-			body.push(chunk);
+	} else if (url === "/new-user" && method === "POST") {
+		const data = [];
+		request.on("data", (chunk) => {
+			data.push(chunk);
 		});
-	
-		// Node has parsed all the request
+
 		request.on("end", () => {
-			// We concatenate all the body-chunks in a string
-			const data = Buffer.concat(body).toString();
-			// Retrieve the data
-			const text = data.split('=')[1];
-			// Save in file
-			fs.writeFileSync("test-form-value", text);
+			const parsedData = Buffer.concat(data).toString();
+			const text = parsedData.split("=")[1];
+			
+			fs.writeFileSync("./text", text);
 		});
-	
-		// Tell the browser we want to redirect the client
-		// And where to
+
 		response.statusCode = 302;
 		response.setHeader('Location', '/');
-		return response.end();
-	} else {
-		response.setHeader("Content-Type", "text/html");
-		response.write(`
-		<html>
-			<head><title>Sup motherfucker</title></head>
-			<body>
-				<h1>Hello Motherfucker!!!!</h1>
-			</body>
-		</html>
-		`);
-	
-		// We return the function so no more code is executed
 		return response.end();
 	}
 }
 
-// HOW TO EXPORT
-// Method: 1
-module.exports = requestHandler;
+exports = requestHandler;
